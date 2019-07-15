83466ceef4c187662b7659af4830d1b69d3b1db7
Michael Day <day.michael.b@gmail.com>
2019-07-15 12:45:18 -0500

    getplates not divisible green

--- model/calc.py
+++ model/calc.py
@@ -3,6 +3,10 @@ def get_plates(weight, sizes=[45,25,10,5,2.5], bar=45):
     weights = dict()
     if weight - bar < 0:
         raise LessThanBar
+    plate_weight = weight - bar
+    smallest_plate = min(sizes)
+    if plate_weight % smallest_plate > 0:
+        raise NotDivisible
     return weights
 
 class LessThanBar(ValueError):
