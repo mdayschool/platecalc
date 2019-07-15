8f94a8abed10b3a3aa1b78ad96f1d916698be00e
Michael Day <day.michael.b@gmail.com>
2019-07-15 12:24:24 -0500

    getplates less bar red

--- model/calc.py
+++ model/calc.py
@@ -3,3 +3,6 @@ def get_plates(weight, sizes=[45,25,10,5,2.5], bar=45):
     weights = dict()
     return weights
 
+class LessThanBar(ValueError):
+    pass
+
--- test/test_calc.py
+++ test/test_calc.py
@@ -4,4 +4,7 @@ from model import calc
 class CalcTestCase(unittest.TestCase):
     def test_getplates_empty(self):
         empty_dict = dict();
-        self.assertEqual(empty_dict, calc.get_plates(0))
+        self.assertEqual(empty_dict, calc.get_plates(45))
+    def test_getplates_less_than_bar(self):
+        with self.assertRaises(calc.LessThanBar):
+            calc.get_plates(25)
