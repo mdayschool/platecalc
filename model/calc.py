579d1b27f5156604eaa9417fc85923cc200daf10
Michael Day <day.michael.b@gmail.com>
2019-07-15 12:42:17 -0500

    getplates not divisible red

--- model/calc.py
+++ model/calc.py
@@ -8,3 +8,6 @@ def get_plates(weight, sizes=[45,25,10,5,2.5], bar=45):
 class LessThanBar(ValueError):
     pass
 
+class NotDivisible(ValueError):
+    pass
+
--- test/test_calc.py
+++ test/test_calc.py
@@ -5,6 +5,11 @@ class CalcTestCase(unittest.TestCase):
     def test_getplates_empty(self):
         empty_dict = dict();
         self.assertEqual(empty_dict, calc.get_plates(45))
+
     def test_getplates_less_than_bar(self):
         with self.assertRaises(calc.LessThanBar):
             calc.get_plates(25)
+
+    def test_getplates_not_divisible(self):
+        with self.assertRaises(calc.NotDivisible):
+            calc.get_plates(46.25)
