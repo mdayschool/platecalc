f239306caeabf5ded422e72a2c016b7e5fb8259f
Michael Day <day.michael.b@gmail.com>
2019-07-15 13:05:22 -0500

    getplates standard red

--- test/test_calc.py
+++ test/test_calc.py
@@ -13,3 +13,14 @@ class CalcTestCase(unittest.TestCase):
     def test_getplates_not_divisible(self):
         with self.assertRaises(calc.NotDivisible):
             calc.get_plates(46.25)
+
+    def test_getplates_standard_weights_1(self):
+        self.assertEqual({45:1}, calc.get_plates(135))
+
+    def test_getplates_standard_weights_2(self):
+        self.assertEqual({45:1, 25:1}, calc.get_plates(185))
+
+    def test_getplates_standard_weights_3(self):
+        self.assertEqual({45:1, 25:1, 2.5:1}, calc.get_plates(190))
+
+
