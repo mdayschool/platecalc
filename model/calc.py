2eb00f0a598165dba91bfaf610a42b7a75a11803
Michael Day <day.michael.b@gmail.com>
2019-07-15 12:27:36 -0500

    getplates less bar green

--- model/calc.py
+++ model/calc.py
@@ -1,6 +1,8 @@
 
 def get_plates(weight, sizes=[45,25,10,5,2.5], bar=45):
     weights = dict()
+    if weight - bar < 0:
+        raise LessThanBar
     return weights
 
 class LessThanBar(ValueError):
